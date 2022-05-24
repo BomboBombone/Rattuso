f055487113a79fd69d204ee6f00dded5eef1a319
paciaronimarco10@gmail.com <paciaronimarco10@gmail.com>
2022-05-24 19:13:48 +0200

    Aggiungere i file di progetto.

---
+++ ManualMappingTest/main.cpp
@@ -0,0 +1,46 @@
+#include "injection.h"
+
+const char szDllFile[] = "C:\\Users\\bombo\\source\\repos\\ManualMappingTest\\ManualMappingTest\\Test.dll";
+const char szProc[] = "Test_Console.exe";
+
+int main() {
+	PROCESSENTRY32 PE32{ 0 };
+	PE32.dwSize = sizeof(PE32);
+
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnap == INVALID_HANDLE_VALUE) {
+		DWORD Err = GetLastError();
+		printf("CreateToolhelp32Snapshot failed 0x%X\n");
+		system("PAUSE");
+		return 0;
+	}
+
+	DWORD PID = 0;
+	BOOL bRet = Process32First(hSnap, &PE32);
+	while (bRet) {
+		if (!strcmp(szProc, PE32.szExeFile)) {
+			PID = PE32.th32ProcessID;
+			break;
+		}
+		bRet = Process32Next(hSnap, &PE32);
+	}
+
+	CloseHandle(hSnap);
+
+	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID);
+	if (!hProc) {
+		DWORD Err = GetLastError();
+		printf("OpenProcess failed: 0x%X\n");
+		system("PAUSE");
+		return 0;
+	}
+
+	if (!ManualMap(hProc, szDllFile)) {
+		CloseHandle(hProc);
+		printf("Something went wrong...");
+		system("PAUSE");
+		return 0;
+	}
+	CloseHandle(hProc);
+	return 0;
+}
